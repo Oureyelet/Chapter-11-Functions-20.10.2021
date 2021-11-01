a52910d5c523c886b8a12095d042a43f1d096ead
Mateusz <oureyelet@gmail.com>
2021-11-01 19:41:15 +0000

    quiz time done. fail.

--- 11.7 — Function Pointers (vsCode)/QuizTime.cpp
+++ 11.7 — Function Pointers (vsCode)/QuizTime.cpp
@@ -6,20 +6,14 @@ void ignoreLine()
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-typedef int (*fcn_ptr)(int,int); 
 
-int ask_user(std::size_t x, std::size_t y, char c)
+int getInteger()
 {
-    /*
-    std::size_t x{};
-    std::size_t y{}; 
-    char c{};
-    */
-
-    std::cout << "Enter forst integer: ";
+    int x{};
 
     while (true) // Loop until user enters a valid input
     {
+        std::cout << "Enter an integer: ";
         std::cin >> x;
 
         if (std::cin.fail()) 
@@ -34,51 +28,25 @@ int ask_user(std::size_t x, std::size_t y, char c)
             break;
         }
     }
-    std::cout << '\n';
 
-    std::cout << "Enter second mathematical operation (‘+’, ‘-‘, ‘*’, ‘/’): ";
-    while (true)
-    {
-        std::cin >> c;
-        ignoreLine();
-
-        switch (c)
-        {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            std::cout << '\n';
-            std::cout << "Your input is: " << x << ' ' << y << ' ' << c << '\n';
-            break;
-
-        default: // otherwise tell the user what went wrong
-            std::cout << "Oops, that input is invalid.  Please try again.\n";
-        }
-        break;
-    }
-    std::cout << '\n';
-
-
-    std::cout << "Enter second integer: ";
-
-    while (true) // Loop until user enters a valid input
-    {
-        std::cin >> y;
-
-        if (std::cin.fail()) 
-        {
-            std::cin.clear();
-            ignoreLine();
-            std::cout << "Oops, that input is invalid.  Please try again.\n";
-        }
-        else
-        {
-            ignoreLine();
-            break;
-        }
-    }
+    return x;
 }
+
+char getOperation()
+{
+    char op{};
+
+    do
+    {
+        std::cout << "Enter an operation ('+', '-', '*', '/'): ";
+        std::cin >> op;
+        ignoreLine();
+    } 
+    while (op!='+' && op!='-' && op!='*' && op!='/');
+    
+    return op;
+}
+
 
 int add(int x, int y)
 {
@@ -100,19 +68,20 @@ int division(int x, int y)
     return x / y;
 }
 
+typedef int (*fcn_ptr)(int,int); 
 
 fcn_ptr getArithmeticFunction(char c)
 {
     switch (c)
     {
     case '+':
-        return add;
+        return &add;
     case '-':
-        return subtract;
+        return &subtract;
     case '*':
-        return multiply;
+        return &multiply;
     case '/':
-        return division;
+        return &division;
     }
     return nullptr;
 }
@@ -134,21 +103,17 @@ int main()
     1a) Create a short program asking the user for two integer inputs and a mathematical operation 
     (‘+’, ‘-‘, ‘*’, ‘/’). Ensure the user enters a valid operation.
     */
-    //ask_user();
-
+    //getInteger();
+    //getOperation();
 
     /*
     1b) Write functions named add(), subtract(), multiply(), and division(). These should take two integer parameters 
     and return an integer.
     */
     //std::cout << add(3, 3) << '\n';
-
     //std::cout << subtract(3, 3) << '\n';
-
     //std::cout << multiply(3, 3) << '\n';
-
     //std::cout << division(3, 3) << '\n';
-
 
     /*
     1d) Write a function named getArithmeticFunction() that takes an operator character and returns the appropriate 
@@ -156,11 +121,18 @@ int main()
     */
     //getArithmeticFunction('*');
 
-
     /*
     1e) Modify your main() function to call getArithmeticFunction(). Call the return value from that function 
     with your inputs and print the result.
     */
+    int x{ getInteger() };
+    char op{ getOperation() };
+    int y{ getInteger() };
+
+
+    fcn_ptr fcn{ getArithmeticFunction(op) };
+
+    std::cout << x << ' ' << op << ' ' << y << " = " << fcn(x, y) << '\n';
 
 
 
