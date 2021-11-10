aa4ff08fa531962afdb181258b2c9636fa8a9161
Mateusz <oureyelet@gmail.com>
2021-11-10 20:07:27 +0000

    Command line arguments

--- 11.11 — Command line arguments (vsCode)/main.cpp
+++ 11.11 — Command line arguments (vsCode)/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello World" << '\n';
 
@@ -72,8 +72,149 @@ int main()
     std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
     ////////////////////////////////////////////////////////////////////////////////////////////
     /*
+    Executable programs can be run on the command line by invoking them by name. For example, to run the 
+    executable file “WordCount” that is located in the root directory of the C: drive on a Windows machine, you could type:
+    
+    C:\>WordCount
 
+    In order to pass command line arguments to WordCount, we simply list the command line arguments after the executable name:
+
+    C:\>WordCount Myfile.txt
+
+    Now when WordCount is executed, Myfile.txt will be provided as a command line argument. A program can have multiple 
+    command line arguments, separated by spaces:
+
+    C:\>WordCount Myfile.txt Myotherfile.txt
+
+    This also works for other command line operating systems, such as Linux (although your prompt and directory structure 
+    will undoubtedly vary).
+
+    If you are running your program from an IDE, the IDE should provide a way to enter command line arguments.
+
+    In Microsoft Visual Studio, right click on your project in the solution explorer, then choose properties. Open the 
+    “Configuration Properties” tree element, and choose “Debugging”. In the right pane, there is a line called 
+    “Command Arguments”. You can enter your command line arguments there for testing, and they will be automatically 
+    passed to your program when you run it.
+
+    In Code::Blocks, choose “Project -> Set program’s arguments”.
     */
+
+
+    std::cout << std::endl;
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Using command line arguments" << '\n';
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Now that you know how to provide command line arguments to a program, the next step is to access them from within our 
+    C++ program. To do that, we use a different form of main() than we’ve seen before. This new form of main() 
+    takes two arguments (named argc and argv by convention) as follows:
+    
+    int main(int argc, char* argv[])
+
+    You will sometimes also see it written as:
+
+    int main(int argc, char** argv)
+
+    Even though these are treated identically, we prefer the first representation because it’s intuitively 
+    easier to understand.
+
+    argc is an integer parameter containing a count of the number of arguments passed to the program 
+    (think: argc = argument count). argc will always be at least 1, because the first argument is always the name 
+    of the program itself. Each command line argument the user provides will cause argc to increase by 1.
+
+    argv is where the actual argument values are stored (think: argv = argument values, though the proper name is 
+    “argument vectors”). Although the declaration of argv looks intimidating, argv is really just an array of 
+    C-style strings. The length of this array is argc.
+
+    Let’s write a short program named “MyArgs” to print the value of all the command line parameters:
+    */
+    std::cout << "There are " << argc << " arguments:\n";
+
+    // Loop through each argument and print its number and value
+    for(int count{ 0 }; count < argc; ++count)
+    {
+        std::cout << count << ' ' << argv[count] << '\n';
+    }
+    std::cout << std::endl;
+
+    /*
+    Now, when we invoke this program (MyArgs) with the command line arguments “Myfile.txt” and “100”, the output 
+    will be as follows:
+
+    There are 3 arguments:
+    0 C:\MyArgs
+    1 Myfile.txt
+    2 100
+
+    Argument 0 is the path and name of the current program being run. Argument 1 and 2 in this case are the two 
+    command line parameters we passed in.
+    */
+
+
+    std::cout << std::endl;
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Dealing with numeric arguments" << '\n';
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Command line arguments are always passed as strings, even if the value provided is numeric in nature. 
+    To use a command line argument as a number, you must convert it from a string to a number. Unfortunately, 
+    C++ makes this a little more difficult than it should be.
+
+    The C++ way to do this follows:
+
+    #include <iostream>
+    #include <sstream> // for std::stringstream
+    #include <string>
+
+    int main(int argc, char* argv[])
+    {
+        if (argc <= 1)
+        {
+            // On some operating systems, argv[0] can end up as an empty string instead of the program's name.
+            // We'll conditionalize our response on whether argv[0] is empty or not.
+            if (argv[0])
+                std::cout << "Usage: " << argv[0] << " <number>" << '\n';
+            else
+                std::cout << "Usage: <program name> <number>" << '\n';
+
+            return 1;
+        }
+
+        std::stringstream convert{ argv[1] }; // set up a stringstream variable named convert, initialized with the 
+        input from argv[1]
+
+        int myint{};
+        if (!(convert >> myint)) // do the conversion
+            myint = 0; // if conversion fails, set myint to a default value
+
+        std::cout << "Got integer: " << myint << '\n';
+
+        return 0;
+    }
+
+    When run with input “567”, this program prints:
+
+    Got integer: 567
+
+    std::stringstream works much like std::cin. In this case, we’re initializing it with the value of argv[1], so that 
+    we can use operator>> to extract the value to an integer variable (the same as we would with std::cin).
+
+    We’ll talk more about std::stringstream in a future chapter.
+    */
+
+
+    std::cout << std::endl;
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Dealing with numeric arguments" << '\n';
+    std::cout << "////////////////////////////////////////////////////////////////////" << '\n';
+    ////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
     return 0;
 }
